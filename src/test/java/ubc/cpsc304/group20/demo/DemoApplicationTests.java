19f881154019fe4fd843334968961b864555230c
zhiwenli <lizhiwenzg@gmail.com>
2021-03-25 00:28:48 -0700

    Project Initialization

---
+++ src/test/java/ubc/cpsc304/group20/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ubc.cpsc304.group20.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
